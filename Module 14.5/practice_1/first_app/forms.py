b07979fe9b80eed3fdc818dde8c5b139486a5fef
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2024-02-23 17:31:48 +0600

    Module 14.5 Done

--- Module 14.5/practice_1/first_app/forms.py
+++ Module 14.5/practice_1/first_app/forms.py
@@ -26,23 +26,23 @@ FAV_SPORTS = [
 
 
 class StudentForm(forms.Form):
-    # name = forms.CharField()
-    # comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10}))
-    # email = forms.EmailField(label="Please Enter your email address")
-    # agree = forms.BooleanField(initial=True)
-    # date = forms.DateField(widget=NumberInput(attrs={'type':'date'}))
-    # birth_date = forms.DateField(widget=NumberInput(attrs={'type':'date'}))
-    # birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEARS))
-    # value = forms.DecimalField(required=False)
+    name = forms.CharField()
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10}))
+    email = forms.EmailField(label="Please Enter your email address")
+    agree = forms.BooleanField(initial=True)
+    date = forms.DateField(widget=NumberInput(attrs={'type':'date'}))
+    birth_date = forms.DateField(widget=NumberInput(attrs={'type':'date'}))
+    birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEARS))
+    value = forms.DecimalField(required=False)
 
-    # message = forms.CharField(max_length=10)
+    message = forms.CharField(max_length=10)
 
-    # first_name = forms.CharField(initial="Your First Name")   
-    # todays_date = forms.DateField(initial=datetime.date.today)
+    first_name = forms.CharField(initial="Your First Name")   
+    todays_date = forms.DateField(initial=datetime.date.today)
 
-    # fav_color = forms.ChoiceField(widget=forms.RadioSelect ,choices=FAV_COLORS)
-    # fav_sports = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=FAV_SPORTS, label="Select your Favorite Sports")
-    # fav_sports2 = forms.MultipleChoiceField(choices=FAV_SPORTS, label="Select your Favorite Sports")
+    fav_color = forms.ChoiceField(widget=forms.RadioSelect ,choices=FAV_COLORS)
+    fav_sports = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=FAV_SPORTS, label="Select your Favorite Sports")
+    fav_sports2 = forms.MultipleChoiceField(choices=FAV_SPORTS, label="Select your Favorite Sports")
 
 
 
--- Module 14.5/practice_1/first_app/models.py
+++ Module 14.5/practice_1/first_app/models.py
@@ -14,26 +14,26 @@ class MyModel(models.Model):
 class NewModel(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField()
-    # file = models.FileField(upload_to='uploads/')
+    file = models.FileField(upload_to='uploads/')
     # file_path = models.FilePathField(path='/first_app/')
 
-    # float_val = models.FloatField()
+    float_val = models.FloatField()
     # foreign_key = models.ForeignKey(MyModel, on_delete=models.CASCADE)
 
-    # ipVal = models.GenericIPAddressField()
-    # file = models.FileField()
+    ipVal = models.GenericIPAddressField()
+    file = models.FileField()
 
     # int_val = models.IntegerField()
 
-    # pos_int = models.PositiveBigIntegerField()
-    # pos_bit_int = models.PositiveBigIntegerField()
-    # pos_small_int = models.PositiveSmallIntegerField()
+    pos_int = models.PositiveBigIntegerField()
+    pos_big_int = models.PositiveBigIntegerField()
+    pos_small_int = models.PositiveSmallIntegerField()
 
-    # slug_text = models.SlugField()
-    # small_int = models.SmallIntegerField()
+    slug_text = models.SlugField()
+    small_int = models.SmallIntegerField()
 
-    # text = models.TextField()
-    # time = models.TimeField()
+    text = models.TextField()
+    time = models.TimeField()
 
     # uuid = models.UUIDField()
     
@@ -43,9 +43,9 @@ class NewModel(models.Model):
     # big_integer_field = models.BigIntegerField()
     # text = models.CharField(max_length=255)
 
-    # date = models.DateField()
-    # appointment = models.DateTimeField()
+    date = models.DateField()
+    appointment = models.DateTimeField()
 
-    # value = models.DecimalField(max_digits = 5, decimal_places=2)
-    # duration = models.DurationField()
+    value = models.DecimalField(max_digits = 5, decimal_places=2)
+    duration = models.DurationField()
 
